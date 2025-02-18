b3e3acb6940ebbd32cbb6ddbeeba08ee95dcf81c
Edgar <kuedgardo9@gmail.com>
2025-02-18 10:53:01 -0600

    actualizacion de la arquitectura inicial

---
+++ backend/config/env.js
@@ -0,0 +1,7 @@
+function env_() {
+    return {
+      ID_DATABASE: "1tFjeHhatqY0lGCynJhvFszLSLmqFJxzECVJDBsJHKpU",
+      ID_CARPETA: "12x-1DjnUcxto2JwbLT-JULjsasK84i3x",
+      SH_REGISTRO_USUARIOS: "registro usuarios",
+    };
+  } 
---
+++ backend/database/conexion.js
@@ -0,0 +1,14 @@
+
+function conexion() {
+    return SpreadsheetApp.openById(env_().ID_DATABASE);
+  }
+  
+ 
+  function obtenerSheet(NAME) {
+    return conexion().getSheetByName(NAME);
+  }
+  
+
+  function obtenerDatos(NAME) {
+    return obtenerSheet(NAME).getDataRange().getDisplayValues();
+  }
